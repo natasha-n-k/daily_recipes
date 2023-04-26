42bffd69c48bda06ea9ec563ff69c7a0742f29eb
Natasha <nnkonovalova@bk.ru>
2023-04-26 15:57:35 +0300

    corrected notes, text

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ class EdamamAPI:
             "app_id": self.app_id,
             "app_key": self.app_key,
             "from": 0,
-            "to": 10
+            "to": 5
         }
 
         if vegetarian:
@@ -45,20 +45,20 @@ class EdamamAPI:
     
 
 def start(update, context):
-    """Обработчик команды /start."""
+    """Handler function for the /start command."""
     context.bot.send_message(chat_id=update.effective_chat.id,
-                            text="Привет! Введите список продуктов через запятую, и я найду для вас рецепты блюд. Также можете указать через запятную особенности блюда: вегетарианское, без молока, без глютена")
+                            text="Hi! Send a message to the bot with a list of ingredients separated by commas. You can also add one or more of the following filters: 'vegetarian', 'gluten-free', and 'dairy-free'.")
 
 
 def find_recipes(update, context):
-    # Получаем введенные продукты и определяем язык
+    # Get input ingredients and detect language
     lang = detect(update.message.text)
     query = update.message.text.lower()
     vegetarian = False
     gluten_free = False
     dairy_free = False
 
-    # Проверяем, есть ли в запросе фильтры
+    # Check if filters are included in the query
     if "вегетарианский" in query:
         vegetarian = True
     if "без глютена" in query:
@@ -66,19 +66,18 @@ def find_recipes(update, context):
     if "без молока" in query:
         dairy_free = True
 
-    # Выбираем API-сервис на основе языка
     app_id = "0444e0ee"
     app_key = "d27b186ea5810d2379393198a8096486"
     api = EdamamAPI(app_id, app_key)
 
-    # Получаем список рецептов по запросу
+    # Get recipe list based on the query
     recipes = api.search_recipes(query, vegetarian, gluten_free, dairy_free)
 
-    # Отправляем результат пользователю
+    # Send results back to user
     if not recipes:
-        context.bot.send_message(chat_id=update.effective_chat.id, text='К сожалению, я не смог найти рецептов с такими продуктами и фильтрами.')
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Sorry, I could not find any recipes with these ingredients and dietary restrictions.')
     else:
-        # Отправляем найденные рецепты пользователю
+        # Send found recipes to user
         for recipe in recipes:
             title = recipe["label"]
             url = recipe["url"]
@@ -88,17 +87,17 @@ def find_recipes(update, context):
 
 
 def main():
-    # Инициализация бота
+    #  Initialize the bot
     updater = Updater(token=TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
-    # Регистрация обработчиков
+    # Register the handlers
     start_handler = MessageHandler(Filters.regex('^/start$'), start)
     recipe_handler = MessageHandler(Filters.text, find_recipes)
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(recipe_handler)
 
-    # Запуск бота
+    # Bot launch
     updater.start_polling()
     updater.idle()
 
